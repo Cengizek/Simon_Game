36f8f647591d83b838d71f1270afce31eac554e3
Cengiz <140445156+Cengizek@users.noreply.github.com>
2023-10-30 19:48:17 +0300

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ var level=0;
 
 $(document).keypress(function(){
  if(!started){
-        $("#level-title").text("Level"+level);
+        $("#level-title").text("Level " +level);
         nextSequence();
         started = true;
     }
